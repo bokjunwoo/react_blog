f1b36042c9b973cbdc9518e74747a6ef9e9b544a
JUNU <106523012+bokjunwoo@users.noreply.github.com>
2022-10-19 23:05:49 +0900

    업데이트

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 import { render } from '@testing-library/react';
+import React from 'react';
 
 function App() {
 
-  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남우동 맛집', '리액트공부']);
+  let [글제목, 글제목변경] = useState(['리액트 시작', '임포트', '자습하기']);
   
   let[따봉, 따봉변경] = useState([0, 0, 0]);
 
@@ -86,8 +87,7 @@ function App() {
         copy.unshift(입력값);
         글제목변경(copy)
       }}>글 발행</button>
-      <Modal2></Modal2>
-
+      
       {
         // 조건식 ? 참일때 실행할 코드 : 거짓일 때 실행할 코드
         modal == true ? <Modal 글제목변경={글제목변경} 글제목={글제목} title={title}></Modal> : ''
@@ -111,24 +111,6 @@ function Modal(props) {
       }}>글수정</button>
     </div>
   )
-}
-
-class Modal2 extends React.Component {
-  constructor(){
-    super();
-    this.state = {
-      name: 'kim'
-    };
-  }
-  render(){
-    return (
-      <div>안녕{this.state.name}
-      <button onClick={() => {
-        this.setState({name:'bok'})
-      }}>버튼</button>
-      </div>
-    )
-  }
 }
 
 export default App;
